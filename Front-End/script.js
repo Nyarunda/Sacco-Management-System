b5e329c767603bbc69aaf044022c422da059da45
Nayrunda <ezranyagaka96@gmail.com>
2023-10-20 16:15:55 +0300

    Fixed navbar outohide sidebar

--- Front-End/script.js
+++ Front-End/script.js
@@ -12,3 +12,10 @@ allSideMenu.forEach(item => {
         li.classList.add('active');
     })
 });
+
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sideBar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function() {
+    sideBar.classList.toggle('hide');
+})
